a0b62be6500857388846a90f83be7f96588eb236
fiderosado <fiderosado@gmail.com>
2023-07-09 13:47:04 -0400

    Fix TropipayRequireAuth and add redirect

--- src/classes/TropipayRequireAuth.js
+++ src/classes/TropipayRequireAuth.js
@@ -14,13 +14,39 @@ const TropipayRequireAuth = (params) => {
             }
             try {
                 router = useRouter();
-                if (params.redirectIfNotAuthenticated && !params.session.id) {
-                    if (!params.redirectTo) {
-                        console.warn('Please provide redirectTo as redirection url');
-                    }
-                    router.replace(params.redirectTo);
+                let errores = [];
+                if (!(typeof params?.redirectIfNotAuthenticated === "boolean")) {
+                    errores.push("Error: Faltan parametros: redirectIfNotAuthenticated ned to be a boolean");
                 }
-            } catch (err) {}
+
+                if (!(typeof params?.redirectTo === "string")) {
+                    errores.push('Error: Please provide redirectTo as redirection url');
+                }
+
+                if (errores.length > 0) {
+                    throw new Error(`${errores.join(', ')}`)
+                }
+
+                if (
+                    !(typeof params?.redirectIfNotAuthenticated === "object") &&
+                    !(typeof params?.session?.id === "string")
+                ) {
+                    if (params.redirectIfNotAuthenticated) {
+                        if (!params.redirectTo) {
+                            throw new Error('Please provide redirectTo as redirection url');
+                        }
+                        //router.replace(params.redirectTo);
+                        const redirectComponent = React.createElement("script", {
+                            dangerouslySetInnerHTML: {
+                                __html: `window.location.href = "${params.redirectTo}";`
+                            }
+                        });
+                        return redirectComponent;
+                    }
+                }
+            } catch (err) {
+                console.log("- TropipayRequireAuth: ",err);
+            }
             return React?.createElement(WrappedComponent, newparams);
         }
         return WithAuthComponent;
