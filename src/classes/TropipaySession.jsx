d80df6af57d56ee135fe7792737cbd0903dcca89
fiderosado <fiderosado@gmail.com>
2023-07-10 17:32:00 -0400

    Adding crypto-js

---
+++ src/classes/TropipaySession.jsx
@@ -0,0 +1,18 @@
+// TropipaySession
+Object.defineProperty(exports, "__esModule", {value: true});
+const TropipaySession = () => {
+
+    const CreateClientSession = function () {
+        return true
+    }
+
+    const GetSession = function () {
+        return {}
+    }
+
+    return {
+        CreateClientSession,
+        GetSession,
+    }
+}
+module.exports = TropipaySession;
